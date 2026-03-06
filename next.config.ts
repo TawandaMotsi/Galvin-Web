8ae946b8fe133c44d2dd1fa17ec9652744ee49da
emottaw <tawanda.bradreck.motsi@ericsson.com>
2026-03-06 15:56:57 +0000

    Add assetPrefix for static assets

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/Galvin-Web',
+  assetPrefix: '/Galvin-Web',
   reactCompiler: true,
   images: {
     unoptimized: true,
